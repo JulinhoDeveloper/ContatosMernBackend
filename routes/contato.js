c0ff4b13010f18152849616a5d4bda311c0c423f
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-02-05 23:10:29 -0300

    exclusão de contatos

--- routes/contato.js
+++ routes/contato.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const  {validarContato, Contato} = require("../models/Contatos");
+const mongoose = require("mongoose");
 const auth = require("../middlewares/auth");
 
 //criar contato
@@ -37,6 +38,28 @@ router.get("/meuscontatos", auth, async (req,res)=>{
         const meusContatos = await Contato.find({ postedBy: req.user._id }).populate("postedBy","-password");
 
         return res.status(200).json({ contatos :meusContatos})
+    } catch (err){
+         console.log(err)
+     }
+});
+
+//excluir contatos
+router.delete("/delete/:id", auth, async (req,res)=>{
+ const { id } = req.params;
+
+ if(!id) return res.status(400).json({error: "id não cadastrado"});
+
+if(!mongoose.isValidObjectId(id))  return res.status(400).json({error: "digite um id válido"});
+    try{
+       const contato = await Contato.findOne( { _id: id });
+       if(!contato)  return res.status(400).json({error: "nenhum contato encontrado"});
+
+       if(req.user._id.toString()  !== contato.postedBy._id.toString() ) 
+        return res.status(401).json({error: "você não pode deletar contato de outros usuários" });
+
+        const result = await Contato.deleteOne({  _id:id });
+        return res.status(200).json({
+            ...contato._doc });
     } catch (err){
          console.log(err)
      }
